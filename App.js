dae75268ce3a1fd54baa54b7bb3fc1d94999b3d6
Neha Kumar <78813871+nehajeryal@users.noreply.github.com>
2021-03-27 21:49:11 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,56 @@
+import React , {useState,useEffect} from 'react';
+import alanBtn from '@alan-ai/alan-sdk-web';
+
+import wordsToNumbers from 'words-to-numbers';
+import NewsCards from'./components/NewsCards/NewsCards';
+import useStyles from './styles.js';
+
+
+const alanKey='ab46a8f4adf9073859be032a3ebe68412e956eca572e1d8b807a3e2338fdd0dc/stage'
+
+const App=()=> {
+     const[newsArticles,setNewsArticles]=useState([]);
+     const [activeArticle,setActiveArticle]=useState(-1);
+     const classes=useStyles();
+
+     useEffect(()=>{
+      alanBtn({
+
+          key:alanKey,
+          onCommand:({command,articles,number})=>{
+
+              if(command==='newHeadlines'){
+                    setNewsArticles(articles);
+                    setActiveArticle(-1);
+               } else if (command==='highlight') {
+                   setActiveArticle((prevActiveArticle)=>prevActiveArticle + 1);
+               } else if (command==='open'){
+
+                //three=>3
+                //four//twenty
+                   const parsedNumber=number.length > 2 ? wordsToNumbers(number,{fuzzy:true}):number;
+                   const article=articles[parsedNumber-1];
+
+                   if (parsedNumber> 20){
+                       alanBtn().playText('please try that again')
+                   } else if (article){
+                   window.open(article.url,'_blank');
+                   alanBtn.playText('Opening...');
+               }   
+            }
+        }
+        
+       })
+  },   [])
+  return (
+         <div>
+            <div className={classes.logoContainer}>
+                 <img src="https://repository-images.githubusercontent.com/217049587/3e07cd00-fa7f-11e9-86bd-123b47b846c9"  className={classes.alanLogo} alt="alan logo"/>
+            </div>
+               
+               <NewsCards articles={newsArticles} activeArticle={activeArticle} />
+         </div>
+  );    
+}
+
+export default App;
